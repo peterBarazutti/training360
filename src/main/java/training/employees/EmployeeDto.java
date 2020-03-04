6b6a97676a84ffc2a445fe2a3cbdba1614cfcb28
peterBarazutti <barazutti.peter@gmail.com>
2020-03-04 14:18:42 +0100

    implementation added

--- src/main/java/training/employees/EmployeeDto.java
+++ src/main/java/training/employees/EmployeeDto.java
@@ -1,4 +1,16 @@
+
 package training.employees;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeDto {
+
+    private long id;
+
+    private String name;
 }
